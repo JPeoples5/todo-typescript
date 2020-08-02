032d4efd9a3cce5ec8a6996ff4f2edca8197a767
Jeremiah Peoples <jeremiah@jeremiahs-mbp.home>
2020-08-02 01:27:36 -0700

    remove boiler plate

---
+++ todo-typescript/src/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+export const Header = () => {
+  return (
+    <div>
+      <header className="header">
+        <span>
+          Todo List 
+          </span>
+      </header>
+    </div>
+  );
+}
+
